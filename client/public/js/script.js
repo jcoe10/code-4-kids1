8bd786cfbc1c5b5f94c662f8369885d2d57192a7
jcoe10 <jacob.e.coe@gmail.com>
2017-11-07 12:49:04 -0500

    changed procfile

--- client/public/js/script.js
+++ client/public/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     // Create a function that creates the start button and initial screen
-    
+
     function initialScreen() {
         startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
         $(".mainArea").html(startScreen);
@@ -11,60 +11,60 @@ $(document).ready(function() {
     //Create a function, generateHTML(), that is triggered by the start button, and generates the HTML seen on the project video...
     
     $("body").on("click", ".start-button", function(event){
-        event.preventDefault();  // added line to test issue on GitHub Viewer
+        event.preventDefault();
         clickSound.play();
-        generateHTML();
+        genHTML();
     
-        timerWrapper();
+        wrapper();
     
-    }); // Closes start-button click
+    });
     
     $("body").on("click", ".answer", function(event){
-        //answeredQuestion = true;
+       
         clickSound.play();
         selectedAnswer = $(this).text();
         if(selectedAnswer === correctAnswers[question]) {
-            //alert("correct");
+            alert("correct");
     
             clearInterval(theClock);
             generateWin();
         }
         else {
-            //alert("wrong answer!");
+            alert("wrong answer!");
             clearInterval(theClock);
             generateLoss();
         }
-    }); // Close .answer click
+    });
     
     $("body").on("click", ".reset-button", function(event){
         clickSound.play();
-        resetGame();
-    }); // Closes reset-button click
+        reset();
+    });
     
-    });  //  Closes jQuery wrapper
+    });
     
-    function generateLossDueToTimeOut() {
+    function genTimeout() {
         unanswered++;
         gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[question] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
         $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000);  //  change to 4000 or other amount
+        setTimeout(wait, 4000);
     }
     
     function generateWin() {
         correct++;
         gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[question] + "</p>" + imageArray[question];
         $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000);  //  change to 4000 or other amount
+        setTimeout(wait, 4000);
     }
     
     function generateLoss() {
         incorrect++;
         gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[question] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
         $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000); //  change to 4000 or other amount
+        setTimeout(wait, 4000);
     }
     
-    function generateHTML() {
+    function genHTML() {
         gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[question] + "</p><p class='first-answer answer'>A. " + answerArray[question][0] + "</p><p class='answer'>B. "+answerArray[question][1]+"</p><p class='answer'>C. "+answerArray[question][2]+"</p><p class='answer'>D. "+answerArray[question][3]+"</p>";
         $(".mainArea").html(gameHTML);
     }
@@ -72,21 +72,21 @@ $(document).ready(function() {
     function wait() {
         if (question < 3) {
         question++;
-        generateHTML();
+        genHTML();
         counter = 30;
-        timerWrapper();
+        wrapper();
         }
         else {
             finalScreen();
         }
     }
     
-    function timerWrapper() {
+    function wrapper() {
         theClock = setInterval(thirtySeconds, 1000);
         function thirtySeconds() {
             if (counter === 0) {
                 clearInterval(theClock);
-                generateLossDueToTimeOut();
+                genTimeout();
             }
             if (counter > 0) {
                 counter--;
@@ -100,14 +100,14 @@ $(document).ready(function() {
         $(".mainArea").html(gameHTML);
     }
     
-    function resetGame() {
+    function reset() {
         question = 0;
         correct = 0;
         incorrect = 0;
         unanswered = 0;
         counter = 30;
-        generateHTML();
-        timerWrapper();
+        genHTML();
+        wrapper();
     }
     
     var startScreen;
@@ -119,10 +119,6 @@ $(document).ready(function() {
         "What is the highest level of a header tag?", 
         "What part do you see on the page?", 
         "What is the current version of HTML?"
-        // "What is the capital of China?", 
-        // "What is the capital of Turkey?", 
-        // "What is the capital of Colombia?", 
-        // "What is the capital of India?"
     ];
     
     var answerArray = 
@@ -131,10 +127,6 @@ $(document).ready(function() {
         ["h3","h1","div","center"], 
         ["footer", "head", "body", "title"], 
         ["HTML5", "HTML2017", "HTMLv5.1", "HTMLC"]
-        // ["Hong Kong", "Macau", "Shanghai", "Beijing"], 
-        // ["Ankara","Istanbul","Antalya","Bursa"], 
-        // ["Medellin", "Bogota", "Cartagena", "Cali"], 
-        // ["Mumbai","Hyderabad","Bangalore","New Delhi"]
     ];
     
     var imageArray = 
@@ -143,10 +135,6 @@ $(document).ready(function() {
         "<img class='center-block img-right' src='css/question2html.png'>", 
         "<img class='center-block img-right' src='css/question3html.png'>", 
         "<img class='center-block img-right' src='css/question4html.png'>"
-        // "<img class='center-block img-right' src='img/china.png'>", 
-        // "<img class='center-block img-right' src='img/turkey.png'>", 
-        // "<img class='center-block img-right' src='img/colombia.png'>", 
-        // "<img class='center-block img-right' src='img/india.png'>"
     ];
     
     var correctAnswers = 
@@ -155,10 +143,6 @@ $(document).ready(function() {
         "B. h1", 
         "C. body", 
         "A. HTML5"
-        // "D. Beijing", 
-        // "A. Ankara", 
-        // "B. Bogota", 
-        // "D. New Delhi"
     ];
 
     var question = 0;
